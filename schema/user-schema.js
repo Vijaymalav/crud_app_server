06143a466c1369a471cd3b4b71bdf9bd6a4e3608
VijayMalav <vijaymalav5455@gmail.com>
2022-07-28 21:19:06 +0530

    mongo fix

--- schema/user-schema.js
+++ schema/user-schema.js
@@ -1,7 +1,6 @@
 
 
 import mongoose from "mongoose";
-import autoIncrement from 'mongoose-auto-increment';
 const userSchema=mongoose.Schema({
     name:String,
     username:String,
@@ -9,8 +8,6 @@ const userSchema=mongoose.Schema({
     phone:String
 })
 
-autoIncrement.initialize(mongoose.connection);
-userSchema.plugin(autoIncrement.plugin,'user');
 
 const user=mongoose.model('user',userSchema);
 export default user;
